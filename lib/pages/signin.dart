569b4fc4defab77d9149db2c7787eed3587b9d1d
San Choo <littlecono1@gmail.com>
2024-05-07 13:43:56 +0545

    sign in debug

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -29,104 +29,112 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100, // Adjusted toolbarHeight
-        title: Text(
-          'Sign In',
-          style: TextStyle(
-              color: Colors.black, fontSize: 40, fontWeight: FontWeight.w500),
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false, // Display back button automatically
+        // Display back button automatically
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              height: 55,
-              width: 300,
-              child: TextField(
-                controller: emailController,
-                enabled: true,
-                decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                  hintText: 'Email',
-                ),
+      body: Container(
+        height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 100),
+              child: Text(
+                'Sign In',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w500),
               ),
             ),
-          ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.only(bottom: 10),
-              width: 300,
-              child: TextField(
-                controller: passwordController,
-                enabled: true,
-                obscureText: true,
-                decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                  hintText: 'Password',
-                ),
-              ),
-            ),
-          ),
-          Container(
-            alignment: Alignment.topRight,
-            margin: EdgeInsets.only(right: 50),
-            child: Text(
-              'Forgot Password?',
-              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500),
-            ),
-          ),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: SizedBox(
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Navigate to the LandingPage when the login button is pressed
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LandingPage()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
+            Center(
+              child: Container(
+                height: 55,
+                width: 300,
+                child: TextField(
+                  controller: emailController,
+                  enabled: true,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                    hintText: 'Email',
                   ),
                 ),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(bottom: 10),
+                width: 300,
+                child: TextField(
+                  controller: passwordController,
+                  enabled: true,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                    hintText: 'Password',
+                  ),
                 ),
               ),
             ),
-          ),
-          RichText(
-            text: TextSpan(
-              text: "Don't have an account? ",
-              style: TextStyle(color: const Color.fromARGB(255, 75, 75, 75)),
-              children: [
-                TextSpan(
-                  text: 'Sign Up',
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.bold),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      // Navigate to the SignUpPage when the text is tapped
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUp()),
-                      );
-                    },
-                ),
-              ],
+            Container(
+              alignment: Alignment.topRight,
+              margin: EdgeInsets.only(right: 50),
+              child: Text(
+                'Forgot Password?',
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.w500),
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: SizedBox(
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigate to the LandingPage when the login button is pressed
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LandingPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                text: "Don't have an account? ",
+                style: TextStyle(color: const Color.fromARGB(255, 75, 75, 75)),
+                children: [
+                  TextSpan(
+                    text: 'Sign Up',
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        // Navigate to the SignUpPage when the text is tapped
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUp()),
+                        );
+                      },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
