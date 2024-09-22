9154acbbfcf661b6ecbbd4259058f09b0fd33e9d
puuq <littlecono1@gmail.com>
2024-09-22 14:25:00 +0545

    programs

--- lib/pages/subpages/ProgramsScreen.dart
+++ lib/pages/subpages/ProgramsScreen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-class ProgramsScreen extends StatelessWidget {
+class ProgramsScreen extends StatefulWidget {
   const ProgramsScreen({super.key});
 
   @override
+  State<ProgramsScreen> createState() => _ProgramsScreenState();
+}
+
+class _ProgramsScreenState extends State<ProgramsScreen> {
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('ProgramsScreen'),
+      body: Center(
+        child: Text('Hello'),
+      ),
     );
   }
 }
